5be51b136ca866f0f2332d643b7aa87f67d312f6
Kai Gothe <kaigostudio@gmail.com>
2019-03-28 00:20:57 +0100

    add debug api method

--- routes/api/Course.js
+++ routes/api/Course.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/get_Name_ID', function(req, res) {
+    console.log(req.body);
     var AcademicDegree = req.body.AcademicDegree;
     dataHandler.get_Name_ID(AcademicDegree, function(err, apiResult){
         if(err){
