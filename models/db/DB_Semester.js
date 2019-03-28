cef30a90ea397971f2aa50d9768b67375f508287
Kai Gothe <kaigostudio@gmail.com>
2019-03-28 16:49:28 +0100

    update Semester select

--- models/db/DB_Semester.js
+++ models/db/DB_Semester.js
@@ -2,9 +2,14 @@ const db = require("../../dbConnection")
 
 const Semester = {
 
-    get_Name_ID:function(callback){
+    get_Name_ID:function(course, callback){
+        return db.query("SELECT DISTINCT SeminarGroup.semester, Semester.name FROM `SeminarGroup` LEFT JOIN Semester ON (SeminarGroup.semester = Semester.fachSemesterID) WHERE SeminarGroup.course = ?", [course], callback);
+    },
+
+    
+    get_all_Name_ID:function(callback){
         return db.query("SELECT `fachSemesterID`, `name` FROM `Semester` WHERE 1", callback);
-    }
+    },
 
 }
 
--- routes/api/Semester.js
+++ routes/api/Semester.js
@@ -3,14 +3,20 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/get_Name_ID', function(req, res) {
-    dataHandler.get_Name_ID(function(err, apiResult){
-        if(err){
-            console.log(err);
-            res.send(err);
-        }else{
-            res.send(JSON.stringify(apiResult));
-        }
-    })
-  });
+
+    var current_course = req.body.Course
+
+    if(current_course){
+        dataHandler.get_Name_ID(current_course,function(err, apiResult){
+            if(err){
+                console.log(err);
+                res.send(err);
+            }else{
+                res.send(JSON.stringify(apiResult));
+            }
+        })
+    }
+});
+    
 
   module.exports = router;
