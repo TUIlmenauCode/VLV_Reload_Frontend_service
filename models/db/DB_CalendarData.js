c0e81edc2fd7721f7a536da28dd52c7065cea330
Kai Gothe <kaigostudio@gmail.com>
2019-03-29 16:25:27 +0100

    Update DB_CalendarData.js

--- models/db/DB_CalendarData.js
+++ models/db/DB_CalendarData.js
@@ -7,7 +7,7 @@ const db = require("../../dbConnection");
 const CalendarData = {
 
     get__Termins__from__CoursID_Date:function(courseID, date,  callback){
-        return db.query("SELECT Termin.start, Termin.end, Room.name AS 'room', `Event`.`title` FROM GroupTermins, SeminarGroup, Termin, Room, `Event` WHERE SeminarGroup.seminarGroupID = ? AND GroupTermins.group = SeminarGroup.seminarGroupID AND Termin.terminID = GroupTermins.termin AND Room.roomID = Termin.room AND `Event`.`eventID` = Termin.event AND date(Termin.start) = ?", 
+        return db.query("SELECT DATE_FORMAT(Termin.start, '%Y-%m-%dT%H:%i:%S'), DATE_FORMAT(Termin.end, '%Y-%m-%dT%H:%i:%S'), Room.name AS 'room', `Event`.`title` FROM GroupTermins, SeminarGroup, Termin, Room, `Event` WHERE SeminarGroup.seminarGroupID = ? AND GroupTermins.group = SeminarGroup.seminarGroupID AND Termin.terminID = GroupTermins.termin AND Room.roomID = Termin.room AND `Event`.`eventID` = Termin.event AND date(Termin.start) = ?", 
         [courseID, date],
         callback);
     }
