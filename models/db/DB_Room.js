7a656f727909de196d7cd47b0076cda7e6bd7946
Kai Gothe <kaigostudio@gmail.com>
2019-04-16 10:51:08 +0200

    bug fix

--- models/db/DB_Room.js
+++ models/db/DB_Room.js
@@ -3,7 +3,7 @@ const db = require("../../dbConnection");
 
 const Room = {
     selectAll__Name_ID:function(callback){
-        return db.query("SELECT roomID, name FROM `Room` WHERE 1");
+        return db.query("SELECT roomID, name FROM `Room` WHERE 1", callback);
     }
 }
 
