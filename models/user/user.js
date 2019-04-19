033d1691cf1920dfacceba5029229525a24e62fe
Kai Gothe <kaigostudio@gmail.com>
2019-04-19 14:12:11 +0200

    test

--- models/user/user.js
+++ models/user/user.js
@@ -113,6 +113,7 @@ const User = {
                                     userName : user_Name, 
                                     userEmail : user_email
                                 }
+                                console.log("Dump result")
                                 console.log(resObj);
                                 callback(errorlist, resObj);
 
