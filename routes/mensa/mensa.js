23373d6919c83cb10628ede440f94704fd8d7760
Kai Gothe <kaigostudio@gmail.com>
2019-04-22 00:11:29 +0200

    add mensas views

--- routes/mensa/mensa.js
+++ routes/mensa/mensa.js
@@ -48,4 +48,72 @@ res.render("Mensa_Ehrenberg", data);
 })
 
 
+
+
+router.get("/Cafeteria_Mensa_Ehrenberg", function(req, res, next){
+  var userData = {
+    userId : 0 ,
+    userName : "Ronny Fuchs" ,
+    avatar : ""
+}
+
+  if (req.session.userId){
+    userData.userId = req.session.userId ;
+    userData.userName = req.session.userName;
+    userData.avatar = req.session.avatar;
+  }
+
+var data = {
+    url : utility.domain,
+    page_title : "Cafeteria Mensa Ehrenberg",
+    userData : userData
+}
+res.render("Mensa_Cafe", data);
+})
+
+
+router.get("/Cafeteria_Röntgenbau", function(req, res, next){
+  var userData = {
+    userId : 0 ,
+    userName : "Ronny Fuchs" ,
+    avatar : ""
+}
+
+  if (req.session.userId){
+    userData.userId = req.session.userId ;
+    userData.userName = req.session.userName;
+    userData.avatar = req.session.avatar;
+  }
+
+var data = {
+    url : utility.domain,
+    page_title : "Cafeteria Röntgenbau",
+    userData : userData
+}
+res.render("Mensa_R", data);
+})
+
+
+router.get("/NANOteria", function(req, res, next){
+  var userData = {
+    userId : 0 ,
+    userName : "Ronny Fuchs" ,
+    avatar : ""
+}
+
+  if (req.session.userId){
+    userData.userId = req.session.userId ;
+    userData.userName = req.session.userName;
+    userData.avatar = req.session.avatar;
+  }
+
+var data = {
+    url : utility.domain,
+    page_title : "NANOteria",
+    userData : userData
+}
+res.render("Mensa_Nano", data);
+})
+
+
 module.exports = router;
