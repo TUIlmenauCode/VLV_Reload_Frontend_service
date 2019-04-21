2208154edf8c0bc5e822b7840508819d5e7c69b1
Kai Gothe <kaigostudio@gmail.com>
2019-04-21 14:19:26 +0200

    Order Course Names

--- models/db/DB_Course.js
+++ models/db/DB_Course.js
@@ -3,7 +3,7 @@ const db = require("../../dbConnection")
 const Course = {
 
     get_Name_ID:function(grade, callback){
-        return db.query("SELECT `courseID`,`name` FROM `Course` WHERE `degree` = ?", [grade], callback);
+        return db.query("SELECT `courseID`,`name` FROM `Course` WHERE `degree` = ? ORDER BY `Course`.`name` ASC", [grade], callback);
     }
 
 }
