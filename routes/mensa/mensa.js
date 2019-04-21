ea57b6c2bcc5ec7d3c831a60ee85c54073dde05f
Kai Gothe <kaigostudio@gmail.com>
2019-04-21 20:19:36 +0200

    add mensa overView

--- routes/mensa/mensa.js
+++ routes/mensa/mensa.js
@@ -4,11 +4,25 @@ const utility = require("../../utility");
 
 
 router.get("/", function(req, res, next){
+    
+    var userData = {
+        userId : 0 ,
+        userName : "Ronny Fuchs" ,
+        avatar : ""
+    }
+    
+      if (req.session.userId){
+        userData.userId = req.session.userId ;
+        userData.userName = req.session.userName;
+        userData.avatar = req.session.avatar;
+      }
+    
     var data = {
         url : utility.domain,
-        page_title : "Übersicht Mensa"
+        page_title : "Übersicht Mensa",
+        userData : userData
     }
-    res.render("Mensa/Mensa", data);
+    res.render("Mensa", data);
 })
 
 module.exports = router;
