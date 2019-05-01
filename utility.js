da84943755cc40794be87c9772f754b96b14879b
Kai Gothe <kaigostudio@gmail.com>
2019-05-01 16:43:06 +0200

    Update utility.js

--- utility.js
+++ utility.js
@@ -2,6 +2,12 @@ var utilities = {
 
      domain : "https://app.vlv-reload.de",
 
+     userData : {
+          userId : 0 ,
+          userName : "Ronny Fuchs" ,
+          avatar : ""
+     },
+
 }
 
 module.exports = utilities;
