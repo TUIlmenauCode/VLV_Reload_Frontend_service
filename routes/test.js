318a914e76b7dc4008f458f02d662fa51fa06899
Kai Gothe <kaigostudio@gmail.com>
2019-05-01 16:43:19 +0200

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const utility = require("../utility");
 
 
 router.get("/sessionDump", function(req, res, next){
@@ -13,6 +14,26 @@ router.get("/userAgent", function(req, res, next){
       req.socket.remoteAddress ||
       (req.connection.socket ? req.connection.socket.remoteAddress : null);
     res.send(req.headers['user-agent'] +"<br>" + fullUrl+"<br>" + ip);
+})
+
+router.get("/tc", function(req,res,next){
+  
+  var userData = utility.userData;
+
+  if (req.session.userId){
+    userData.userId = req.session.userId ;
+    userData.userName = req.session.userName;
+    userData.avatar = req.session.avatar;
+  }
+
+  var data = {
+      url : utility.domain,
+      page_title : "erstelle deinen Stundenplan",
+      apiErrors : [],
+      userData : userData,
+    }
+  res.render("TimeTable_Customizer", data);
+
 })
 
 module.exports = router;
